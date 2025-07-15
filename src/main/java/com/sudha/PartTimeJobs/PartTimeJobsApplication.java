df8c86bb17e8147502de1010f837a4565c01fec8
Sudhakar Reddy Thakkolu <sudhakarreddythakkolu975@gmail.com>
2025-07-16 00:04:13 +0530

    Initial commit

---
+++ src/main/java/com/sudha/PartTimeJobs/PartTimeJobsApplication.java
@@ -0,0 +1,13 @@
+package com.sudha.PartTimeJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartTimeJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartTimeJobsApplication.class, args);
+	}
+
+}
